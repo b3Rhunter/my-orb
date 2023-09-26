29c5f33541f34ff66faa5ee1dde753641d5096e0
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-09-26 12:51:57 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import {useState, useEffect} from 'react';
-import {ethers} from 'ethers';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 import ABI from './ABI.json';
 
-const contractAddress = "0x8f71c43008C1EB3f79183A2dB46E85Cac6C3517c"
+const contractAddress = "0x6626ef990C27812d31d5ba46efC3aF91B6043C66";
 
 function App() {
 
   const [connected, setConnected] = useState(false);
   const [name, setName] = useState("");
   const [address, setAddress] = useState(null);
-  const [auction, setAuction] = useState([]);
+  const [beneficiary, setBeneficiary] = useState(false);
+  const [keeper, setKeeper] = useState(false);
+  const [keeperAddress, setKeeperAddress] = useState("")
   const [highestBidder, setHighestBidder] = useState("");
   const [highestBid, setHighestBid] = useState("");
-  const [endOfAuction, setEndOfAuction] = useState("");
   const [currentPrice, setCurrentPrice] = useState("");
   const [balance, setBalance] = useState(0);
-  const [duration, setDuration] = useState("");
   const [bidPrice, setBidPrice] = useState("");
   const [timeLeft, setTimeLeft] = useState(0);
+  const [finalize, setFinalize] = useState(false);
+  const [chat, setChat] = useState([]);
+  const [answer, setAnswer] = useState("");
+  const [question, setQuestion] = useState("");
 
   const connect = async () => {
     try {
@@ -61,9 +65,23 @@ function App() {
       const signer = await provider.getSigner();
       const userAddress = await signer.getAddress();
       const contract = new ethers.Contract(contractAddress, ABI, signer);
-      const _auction = await contract.currentAuction();
-      const getPrice = await contract.declaredValue();
+      const getPrice = await contract.price();
       const getBalance = await contract.balanceOf(userAddress);
+      const _highestBidder = await contract.leadingBidder();
+      const _highestBid = await contract.leadingBid();
+      const _endOfAuction = await contract.auctionEndTime();
+      const _keeper = await contract.keeper();
+      const _beneficiary = await contract.beneficiary();
+      const chatLength = await contract.getChatHistoryLength();
+      const chatMessages = [];
+      for (let i = 0; i < chatLength; i++) {
+        const message = await contract.getChatMessage(i);
+        chatMessages.push({
+          sender: message[0],
+          text: message[1]
+        });
+      }
+      console.log(chatMessages)
       await signer.signMessage("Hello World");
       const { ethereum } = window;
       if (ethereum) {
@@ -78,57 +96,61 @@ function App() {
       }
       setConnected(true);
       setAddress(userAddress)
-      setAuction(_auction)
-      setHighestBidder(_auction[0].toString())
-      setHighestBid(_auction[1].toString())
-      setEndOfAuction(_auction[2].toString());
+      if (_keeper === userAddress) {
+        setKeeper(true)
+      }
+      if (_beneficiary === userAddress) {
+        setBeneficiary(true)
+      }
+      setHighestBidder(_highestBidder.toString())
+      setHighestBid(_highestBid.toString())
       const currentTimestamp = Math.floor(Date.now() / 1000);
-      const remainingTime = _auction[2] - currentTimestamp;
+      const remainingTime = _endOfAuction - currentTimestamp;
       setTimeLeft(remainingTime > 0 ? remainingTime : 0);
-      
-
       setCurrentPrice(getPrice.toString())
       setBalance(getBalance.toString())
-    } catch(error) {
-      console.log(error)
-    }
-  }
-
-  const startAuction = async () => {
-    try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(contractAddress, ABI, signer);
-      const tx = await contract.startAuction(duration);
-      await tx.wait()
-    } catch(error) {
+      setChat(chatMessages);
+      setKeeperAddress(_keeper)
+    } catch (error) {
       console.log(error)
     }
   }
 
   const bid = async () => {
-    try{
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(contractAddress, ABI, signer);
-      const tx = await contract.bid({value: ethers.utils.parseEther(bidPrice)});
-      await tx.wait()
-    } catch(error) {
-      console.log(error)
-    }
-  }
-
-  const endAuction = async () => {
     try {
       const provider = new ethers.providers.Web3Provider(window.ethereum)
       await provider.send("eth_requestAccounts", []);
       const signer = provider.getSigner();
       const contract = new ethers.Contract(contractAddress, ABI, signer);
-      const tx = await contract.endAuction();
+      const tx = await contract.bid({ value: ethers.utils.parseEther(bidPrice) });
       await tx.wait()
-    } catch(error) {
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const answerQuestion = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, ABI, signer);
+      const tx = await contract.answer(answer);
+      await tx.wait()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askQuestion = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, ABI, signer);
+      const tx = await contract.question(question);
+      await tx.wait()
+    } catch (error) {
       console.log(error)
     }
   }
@@ -137,9 +159,8 @@ function App() {
     const interval = setInterval(() => {
       setTimeLeft(prevTime => prevTime > 0 ? prevTime - 1 : 0);
     }, 1000);
-
     return () => clearInterval(interval);
-}, []);
+  }, []);
 
 
 
@@ -149,32 +170,56 @@ function App() {
       {!connected && <button onClick={connect}>connect</button>}
       {connected && <button className='connect'>{name}</button>}
       {connected && (
-      <main>
-      <section className='auction-details'>
-      <p>Highest Bidder: {highestBidder?.substr(0, 6) + "..."}</p>
-      <p>Highest Bid: {highestBid && ethers.utils.formatEther(highestBid)} ETH</p>
-      <p>Auction Ending in: {timeLeft} seconds</p>
-      <p>Price: {ethers.utils.formatEther(currentPrice)}</p>
-    </section>
+        <main>
+          <section className='auction-details'>
+            <p>Keeper: {keeperAddress?.substr(0, 6) + "..."}</p>
+            <p>Highest Bidder: {highestBidder?.substr(0, 6) + "..."}</p>
+            <p>Highest Bid: {highestBid && ethers.utils.formatEther(highestBid)} ETH</p>
+            <p>Auction Ends: {timeLeft} seconds</p>
+          </section>
 
-    <section className='card'>
-      <div className='ui'>
-      <input type='text' value={duration} onChange={(e) => setDuration(e.target.value)} placeholder='set round duration...'/>
-      <button onClick={() => startAuction(duration)}>Start Auction</button>
+          <section className='card'>
+            <div className='ui'>
 
-      <input type='text' value={bidPrice} onChange={(e) => setBidPrice(e.target.value)} placeholder='place bid...'/>
-      <button onClick={() => bid(bidPrice)}>Bid!</button>
-      <button onClick={endAuction}>End Auction</button>
-      </div>
+              <input type='text' value={bidPrice} onChange={(e) => setBidPrice(e.target.value)} placeholder='place bid...' />
+              <button onClick={() => bid(bidPrice)}>Bid!</button>
+              <div className='chat'>
+              <p>chat...</p>
+              {chat && chat.map((message, index) => (
+                <p key={index}>{message.sender.substr(0, 6) + "..."}: {message.text}</p>
+              ))}
+              </div>
 
-      <div className='orb-container'>
-      <section className="stage">
-        <figure className="orb"><span className="shadow"></span></figure>
-      </section>
-      </div>
-      
-    </section>
-      </main>
+              {keeper && (
+                <>
+                  <input
+                    type='text'
+                    value={question}
+                    onChange={(e) => setQuestion(e.target.value)}
+                    placeholder="Ask a question..." />
+                  <button onClick={askQuestion}>Ask</button>
+                </>
+              )}
+              {beneficiary && (
+                <>
+                  <input
+                    type='text'
+                    value={answer}
+                    onChange={(e) => setAnswer(e.target.value)}
+                    placeholder="Answer the question..." />
+                  <button onClick={answerQuestion}>Answer</button>
+                </>
+              )}
+            </div>
+
+            <div className='orb-container'>
+              <section className="stage">
+                <figure className="orb"><span className="shadow"></span></figure>
+              </section>
+            </div>
+
+          </section>
+        </main>
       )}
     </div>
   );
